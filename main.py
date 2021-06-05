c93f8cc98a31e4b31d7d38169789adb16775d426
futomakiyoin <futomakiyoin@gmail.com>
2021-06-05 17:50:52 -0400

    Commenting

--- main.py
+++ main.py
@@ -12,16 +12,18 @@ counterList = [0]*(upperLimit+1)
 for i in range(repetition):
     counterList[random.randint(0, upperLimit)] += 1
 
+# print actual data
 # for index in range(0, len(counterList)):
 #     print(index, counterList[index])
 
-# print information
+# printer function to print info
 
 
 def printer(info, number):
     print('{:13s}'.format(info), number)
 
 
+# print information
 printer("Range:", str(min(counterList)) + " ~ " + str(max(counterList)))
 printer("Delta:", max(counterList) - min(counterList))
 printer("Theoretical:", str(sum(counterList)/len(counterList)/100)+"%")
